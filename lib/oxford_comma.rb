5fbc0b56799c14434e9988067904a353acc71096
Joseph Reilly <reilly4181@gmail.com>
2019-04-09 19:33:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,13 @@
-def oxford_comma(array)
-array.join
 
-end
-
+array = ["apple"]
 def oxford_comma(array)
-array.split("and")
+  if array.length == 2
+  array[-2] << " and "
+  array.join
+  elsif array.length == 1
+  	array.join
+  elsif array.length > 2
+    array[-1].prepend "and "
+	array.join(", ")
+  end
 end
